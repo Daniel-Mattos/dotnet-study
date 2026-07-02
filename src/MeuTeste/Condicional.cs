b4003513cfc28124275c748bd46c29e7e091415a
Daniel-Mattos <danielmattos.90228@gmail.com>
2026-07-01 21:32:37 -0300

    adicionando mais uma condicao e testes

--- src/MeuTeste/Condicional.cs
+++ src/MeuTeste/Condicional.cs
@@ -1,10 +1,12 @@
 public class Condicional
 {
-    public static string VerificarNumero(int x)
-    {
-        if (x > 0)
-            return $"{x} é um número posistivo";
-        else
-            return $"{x} não é um número posistivo";
-    }
+	public static string VerificarNumero(int x)
+	{
+		if (x > 0)
+			return $"{x} é um número posistivo";
+		else if (x == 0)
+			return $"{x} é um número neutro";
+		else
+			return $"{x} não é um número posistivo";
+	}
 }
--- tests/MeuTeste.Tests/CondicionalTest.cs
+++ tests/MeuTeste.Tests/CondicionalTest.cs
@@ -11,4 +11,23 @@ public class CondicionalTest
         // Assert
         Assert.Equal("5 é um número posistivo", resultado);
     }
+    [Fact]
+    public void VerificarNumero_QuandoNegativo_DeveRetornarMensagemCorreta()
+    {
+        // Act
+        string resultado = Condicional.VerificarNumero(-1);
+
+        // Assert
+        Assert.Equal("-1 não é um número posistivo", resultado);
+    }
+
+    [Fact]
+    public void VerificarNumero_QuandoNeutro_DeveRetornarMensagemCorreta()
+    {
+        // Act
+        string resultado = Condicional.VerificarNumero(0);
+
+        // Assert
+        Assert.Equal("0 é um número neutro", resultado);
+    }
 }
